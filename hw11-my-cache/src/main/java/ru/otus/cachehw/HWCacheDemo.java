19ab886b3d050104ce49dd1a3df56eea6fb54a7a
Marat Bekyashev <MBekyashev@sportmasterlab.net>
2026-01-04 21:01:56 +0300

    .

--- hw11-my-cache/src/main/java/ru/otus/cachehw/HWCacheDemo.java
+++ hw11-my-cache/src/main/java/ru/otus/cachehw/HWCacheDemo.java
@@ -58,23 +58,5 @@ public class HWCacheDemo {
 
         Client client = clientService.getClient(clientId).get();
         logger.info("GetSavedClient after GC. Client is {}", client);
-
-        HwCache<String, Integer> cache = new MyCache<>();
-
-        // пример, когда Idea предлагает упростить код, при этом может появиться "спец"-эффект
-        @SuppressWarnings("java:S1604")
-        HwListener<String, Integer> listener = new HwListener<String, Integer>() {
-            @Override
-            public void notify(String key, Integer value, String action) {
-                logger.info("key:{}, value:{}, action: {}", key, value, action);
-            }
-        };
-
-        cache.addListener(listener);
-        cache.put("1", 1);
-
-        logger.info("getValue:{}", cache.get("1"));
-        cache.remove("1");
-        cache.removeListener(listener);
     }
 }
--- hw11-my-cache/src/main/java/ru/otus/crm/service/DbServiceClientImpl.java
+++ hw11-my-cache/src/main/java/ru/otus/crm/service/DbServiceClientImpl.java
@@ -3,6 +3,7 @@ package ru.otus.crm.service;
 import java.util.List;
 import java.util.Optional;
 import lombok.EqualsAndHashCode;
+import lombok.ToString;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import ru.otus.cachehw.HwCache;
@@ -15,6 +16,7 @@ public class DbServiceClientImpl implements DBServiceClient {
     private static final Logger log = LoggerFactory.getLogger(DbServiceClientImpl.class);
 
     @EqualsAndHashCode
+    @ToString
     private static class WeakCacheKey {
         final long id;
 
@@ -32,6 +34,9 @@ public class DbServiceClientImpl implements DBServiceClient {
         this.transactionRunner = transactionRunner;
         this.dataTemplate = dataTemplate;
         this.clientsCache = new MyCache<>();
+        clientsCache.addListener((key, value, action) -> {
+            log.info("key:{}, value:{}, action: {}", key, value, action);
+        });
     }
 
     @Override
